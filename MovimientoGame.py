d9e060de4e218abd67dba1053d494be6331e27a6
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-03-03 22:33:50 +0100

    Versión movimiento: Implementando

---
+++ MovimientoGame.py
@@ -0,0 +1,213 @@
+from flask import Flask
+from flask_ask import Ask, statement, question
+import pygame, sys # También utilizamos el módulo sys
+from queue import Queue
+from threading import Thread
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+# Creamos una cola para compartir información entre los hilos
+queue = Queue()
+
+# Función que maneja el hilo de Pygame
+def game_thread(queue):
+
+    # Tamaño de la pantalla de Pygame en píxeles
+    ANCHO = 800
+    ALTO = 600
+
+    # FPS
+    FPS = 30
+
+    # Paleta de colores en RGB
+    NEGRO = (0,0,0)
+    BLANCO = (255,255,255)
+    ROJO = (255,0,0)
+    H_FA2F2F = (250,47,47)
+    VERDE = (0,255,0)
+    AZUL = (0,0,255)
+    H_50D2FE = (94,210,254)
+    AZUL2 = (64,64,255)
+    class Jugador(pygame.sprite.Sprite):
+
+        # Sprite del jugador
+        def __init__(self):
+
+            # Heredamos el init de la clase Sprite de Pygame
+            super().__init__()
+
+            # Rectángulo (jugador), recordemos que las imágenes en Pygame son rectángulos
+            self.image = pygame.image.load("Imagenes/Personaje.png").convert() # Convertimos la imagen a tipo Pygame para que el rendimiento mejore
+            #self.image.fill(H_FA2F2F) # No indicamos ningún color al sprite porque queremos que se muestre la imagen
+
+            self.image.set_colorkey(VERDE) # Con esta función podemos eliminar el color indicado por parámetro de la imagen
+
+            # Obtiene el rectángulo (sprite) de la imagen del jugador para poder manipularlo
+            self.rect = self.image.get_rect()
+
+            # Centra el rectángulo (sprite)
+            self.rect.center = (ANCHO // 2, ALTO // 2) # Este operador devuelve el resultado (integer) de dividir, redondeando si sale float
+            #self.rect.center = (400,600) # Podemos colocar el rectángulo en la posición inicial que queramos
+
+            # Velocidad del personaje (inicial)
+            self.velocidad_x = 0 # Inicialmente el objeto va a estar quieto
+            self.velocidad_y = 0
+
+        def update(self, movimiento):
+
+            # Velocidad predeterminada cada vuelta del bucle si no pulsas nada
+            # ESTO PARA TRABAJAR POR VOZ SEGURAMENTE NO NOS INTERESA
+            self.velocidad_x = 0 # Con esto se evita que el personaje se mueva de manera indefinida si no estamos pulsando nada
+            self.velocidad_y = 0
+
+            # Mueve el personaje hacia la izquierda
+            if movimiento == 'Left':
+                self.velocidad_x = -50 # Cada vez que se pulse la tecla se moverá 10px a la izquierda
+            
+            # Mueve el personaje hacia la derecha
+            if movimiento == 'Right':
+                self.velocidad_x = 50
+
+            # Mueve el personaje hacia arriba
+            if movimiento == 'Up':
+                self.velocidad_y = -50 # Cada vez que se pulse la tecla se moverá 10px hacia arriba
+            
+            # Mueve el personaje hacia abajo
+            if movimiento == 'Down':
+                self.velocidad_y = 50
+
+            # Actualiza la posición del personaje
+            self.rect.x += self.velocidad_x
+            self.rect.y += self.velocidad_y
+
+            # Limita el margen izquierdo
+            if self.rect.left < 0: # Cada vez que toque el borde izquierdo e intente salir de la pantalla 
+                self.rect.left = 0 # Se ajusta el personaje al borde izquierdo
+
+            # Limita el margen derecho
+            if self.rect.right > ANCHO:
+                self.rect.right = ANCHO
+
+            # Limita el margen inferior
+            if self.rect.bottom > ALTO:
+                self.rect.bottom = ALTO
+
+            # Limita el margen superior
+            if self.rect.top < 0:
+                self.rect.top = 0
+
+    # Iniciación de Pygame, creación de la ventana, título y control de reloj.
+    pygame.init()
+    pantalla = pygame.display.set_mode((ANCHO, ALTO))
+    pygame.display.set_caption("Movimiento")
+    clock = pygame.time.Clock() # Para controlar los FPS
+
+    # Grupo de sprites, instanciación del objeto jugador.
+    sprites = pygame.sprite.Group() # Se agrupan los sprites para que trabajen en un conjunto y se almacene en la variable que queramos
+    jugador = Jugador()
+    sprites.add(jugador) # Al grupo le añadimos jugador, para que tenga la imagen del jugador
+
+    font = pygame.font.SysFont(None, 48)
+
+    # Bucle de juego
+    ejecutando = True
+    while ejecutando:
+
+        # Es lo que especifica la velocidad del bucle de juego
+        clock.tick(FPS)
+
+        # Eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                ejecutando = False
+                #pygame.quit()
+                #sys.exit()
+
+        # Actualización de sprites
+        sprites.update() # Con esto podemos hacer que todos los sprites (imágenes) se vayan actualizando en la pantalla
+
+        # Fondo de pantalla, dibujo de sprites y formas geométricas
+        pantalla.fill(NEGRO) # Establecemos el color de fondo de la pantalla
+        sprites.draw(pantalla) # Dibujamos los sprites en la pantalla
+        pygame.draw.line(pantalla, H_50D2FE, (400, 0), (400, 800), 1)
+        pygame.draw.line(pantalla, AZUL, (0, 300), (800, 300), 1)
+
+        # Actualizamos el contenido de la pantalla
+        pygame.display.flip() # Permite que solo una porción de la pantalla se actualice, en lugar de toda el área de la pantalla. Si no se pasan argumentos, se actualizará la superficie completa.
+
+        if queue.qsize() > 0: # Nos aseguramos que la cola tenga elementos antes de intentar obtener uno de ellos
+
+            mensaje = queue.get_nowait()
+
+            # Si el mensaje es None, salimos del bucle y terminamos el hilo
+            if mensaje is None:
+                break
+            # Si recibimos un mensaje de texto, lo mostramos en la pantalla
+            if isinstance(mensaje, str):
+
+                if mensaje == 'Clean': # Si se recibe el mensaje Clean es para indicarnos que se vacie la ventana de juego
+                    #screen = pygame.display.get_surface()
+                    #screen.fill(NEGRO)
+                    pygame.quit()
+                    sys.exit()
+                    #pygame.display.update()
+                elif mensaje == 'Up' or mensaje == 'Down' or mensaje == 'Left' or mensaje == 'Right': # Si se nos ha pedido mover el personaje
+                    jugador.update(mensaje)
+                    pygame.display.update()
+
+    pygame.quit()
+
+# Creamos el hilo de Pygame y lo iniciamos
+game_thread = Thread(target=game_thread, args=(queue,))
+game_thread.start()
+
+@ask.launch # Para cuando el usuario lanza la skill
+def start_skill():
+    return question('Bienvenido al videojuego Movimiento. Dime en qué dirección quieres moverte.')
+
+# Definimos la ruta para el intent UpIntent
+@ask.intent('UpIntent')
+def arriba():
+    # Indicamos al videojuego en qué dirección queremos que se mueva el personaje
+    queue.put('Up')
+    return question('El personaje se mueve hacia arriba.')
+
+# Definimos la ruta para el intent DownIntent
+@ask.intent('DownIntent')
+def abajo():
+    # Indicamos al videojuego en qué dirección queremos que se mueva el personaje
+    queue.put('Down')
+    return question('El personaje se mueve hacia abajo.')
+
+# Definimos la ruta para el intent RightIntent
+@ask.intent('RightIntent')
+def derecha():
+    # Indicamos al videojuego en qué dirección queremos que se mueva el personaje
+    queue.put('Right')
+    return question('El personaje se mueve hacia la derecha.')
+
+# Definimos la ruta para el intent DownIntent
+@ask.intent('LeftIntent')
+def izquierda():
+    # Indicamos al videojuego en qué dirección queremos que se mueva el personaje
+    queue.put('Left')
+    return question('El personaje se mueve hacia la izquierda.')
+
+@ask.intent('EndgameIntent')
+def endgame():
+    queue.put('Clean')
+    return statement('Fin del juego.')
+
+# Definimos la ruta para la página principal de la aplicación web
+@app.route('/')
+def index():
+    return 'Esta es la homepage del videojuego Movimiento.'
+
+if __name__ == '__main__':
+
+    app.run(debug=False) # Si desactivamos el modo debug evitamos que nos abra 2 ventanas de pygame
+    # Cuando la aplicación web se detiene, enviamos un mensaje None a la cola
+    queue.put(None)
+    # Esperamos a que el hilo de Pygame termine
+    game_thread.join()
